65a689e362fb2869e23967090d748a66b5676e12
calistasalscpw <calistasalsa.cpw@gmail.com>
2025-07-31 15:08:34 +0700

    feat: update edit and delete task

--- routers/task.js
+++ routers/task.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import Task from '../models/tasks.model.js';
+import User from '../models/users.model.js';
 
 const router = Router();
 
@@ -31,5 +32,51 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: 'Failed to get task' });
     }
 });
+
+router.put('/:taskId', async (req, res) => {
+    try {
+      const { taskId } = req.params;
+      const {name, description, progressStatus, dueDate, assignTo} = req.body;
+
+      const updatedTask = await Task.findByIdAndUpdate(taskId, {
+        name,
+        description,
+        progressStatus,
+        dueDate,
+        assignTo
+      }, {
+        returnDocument: "after"
+      })
+      if (!updatedTask) {
+        return res.status(404).json({error: "Task not found."})
+      }
+      res.json(updatedTask);
+    } catch (err){
+      res.status(400).json({message: err.message});
+    }
+})
+
+router.delete("/:taskId", async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.taskId);
+    if(!deletedTask){
+      return res.status(404).json({error: "Task not found"})
+    }
+
+    await User.findByIdAndUpdate(deletedTask.author, {
+      $pull: {
+        tasks: req.params.taskId
+      }
+    })
+
+    res.status(204).send();
+  } catch (err){
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      error: err.message
+    });
+  }
+})
 
 export default router;
