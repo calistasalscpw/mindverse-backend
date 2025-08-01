36c046bb988a7c6a05f6236c997bfb934444479b
calistasalscpw <calistasalsa.cpw@gmail.com>
2025-08-01 10:26:38 +0700

    feat: update status to fetch users

--- routers/user.js
+++ routers/user.js
@@ -19,7 +19,7 @@ const transporter = nodemailer.createTransport({
 router.get('/', async (req, res) => {
     try{
         const users = await User.find({}, 'username email');
-        res.json(users);
+        res.status(200).json(users);
     } catch (err){
         res.status(500).json({message: "An error occured while fetching users."})
     }
